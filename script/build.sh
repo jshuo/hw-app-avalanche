4b765f508bf321fd20625d336fce66cc2784a4b0
Jeff Shuo <jeffshuo@gmail.com>
2022-03-10 21:19:24 +0800

    removed local cp

--- script/build.sh
+++ script/build.sh
@@ -7,5 +7,5 @@ PATH=$(yarn bin):$PATH
 cd -
 babel --source-maps -d lib src
 flow-copy-source -v src lib
-cp -r lib /home/secux/workspace/avalanche-wallet/node_modules/@secux/hw-app-avalanche
-cp -r src /home/secux/workspace/avalanche-wallet/node_modules/@secux/hw-app-avalanche
+# cp -r lib /home/secux/workspace/avalanche-wallet/node_modules/@secux/hw-app-avalanche
+# cp -r src /home/secux/workspace/avalanche-wallet/node_modules/@secux/hw-app-avalanche
