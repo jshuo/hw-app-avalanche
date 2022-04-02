4df7782d1a80569a6e9b9b84169d3f3a9a44574a
jshuo <jeffshuo@gmail.com>
2022-04-02 14:23:56 +0800

    sign message

--- src/Avalanche.js
+++ src/Avalanche.js
@@ -1,7 +1,7 @@
 //@flow
 
 import createHash from 'create-hash';
-import { buildPathBuffer} from "@secux/utility";
+import { buildPathBuffer } from "@secux/utility";
 import { ITransport, StatusCode, TransportStatusError } from "@secux/transport";
 
 function buildTxBuffer(paths: Array<string>, txs: Array<Buffer>, tp: TransactionType, chainId: number) {
@@ -9,22 +9,22 @@ function buildTxBuffer(paths: Array<string>, txs: Array<Buffer>, tp: Transaction
 
   const head = [], data = [];
   for (let i = 0; i < paths.length; i++) {
-      const headerBuffer = Buffer.alloc(4);
-      headerBuffer.writeUInt16LE(tp, 0);
-      headerBuffer.writeUInt16LE(chainId, 2);
+    const headerBuffer = Buffer.alloc(4);
+    headerBuffer.writeUInt16LE(tp, 0);
+    headerBuffer.writeUInt16LE(chainId, 2);
 
-      const path = paths[i];
-      const { pathNum, pathBuffer } = buildPathBuffer(path);
-      // generic prepare can use 3 or 5 path level key to sign
-      if (pathNum !== 5 && pathNum !== 3) throw Error('Invalid Path for Signing Transaction');
+    const path = paths[i];
+    const { pathNum, pathBuffer } = buildPathBuffer(path);
+    // generic prepare can use 3 or 5 path level key to sign
+    if (pathNum !== 5 && pathNum !== 3) throw Error('Invalid Path for Signing Transaction');
 
-      head.push(Buffer.concat([Buffer.from([pathNum * 4 + 4]), headerBuffer, pathBuffer]));
+    head.push(Buffer.concat([Buffer.from([pathNum * 4 + 4]), headerBuffer, pathBuffer]));
 
 
-      // fixed 2 byte length
-      const preparedTxLenBuf = Buffer.alloc(2);
-      preparedTxLenBuf.writeUInt16BE(txs[i].length, 0);
-      data.push(Buffer.concat([preparedTxLenBuf, txs[i]]));
+    // fixed 2 byte length
+    const preparedTxLenBuf = Buffer.alloc(2);
+    preparedTxLenBuf.writeUInt16BE(txs[i].length, 0);
+    data.push(Buffer.concat([preparedTxLenBuf, txs[i]]));
   }
 
   return Buffer.concat([Buffer.from([paths.length]), ...head, ...data]);
@@ -37,6 +37,41 @@ export default class Avalanche {
     transport
   ) {
     this.transport = transport
+  }
+  async signHash(prefixPath,
+    paths,
+    hash: Buffer,
+  ): Promise<{ hash: Buffer, signatures: Map<string, Buffer> }> {
+    const SIGNATURE_LENGTH = 65;
+    let bip32path = [], hashedMsg = [];
+    let resultMap: Map<string, Buffer> = new Map();
+    for (let i = 0; i < paths.length; i++) {
+      let suffix = paths[i]
+      bip32path[i] = `${prefixPath}/${suffix}`
+      hashedMsg[i] = hash
+    }
+
+    const hashBuffer = buildTxBuffer(bip32path, hashedMsg);
+    const rsp = await this.transport.Send(0x70, 0xa4, 0, 0,
+      Buffer.concat([hashBuffer]));
+    if (rsp.status !== StatusCode.SUCCESS) throw new TransportStatusError(rsp.status);
+
+    if (rsp.dataLength !== SIGNATURE_LENGTH * hashedMsg.length) throw Error('Invalid length Signature');
+    let signature = []
+    let offset = 0
+    while (offset < rsp.dataLength) {
+      const sig = rsp.data.slice(offset, offset + SIGNATURE_LENGTH)
+      offset = offset + SIGNATURE_LENGTH
+      signature.push(sig)
+    }
+
+    for (let i = 0; i < paths.length; i++) {
+      resultMap.set(paths[i].toString(true), signature[i]);
+    }
+    return {
+      hash: hashedMsg,
+      signatures: resultMap
+    }
   }
 
   async signTransaction(
@@ -55,18 +90,18 @@ export default class Avalanche {
 
     const txBuffer = buildTxBuffer(bip32path, hashedTx);
     const rsp = await this.transport.Send(0x70, 0xa4, 0, 0,
-        Buffer.concat([txBuffer]));
-        if (rsp.status !== StatusCode.SUCCESS) throw new TransportStatusError(rsp.status);
+      Buffer.concat([txBuffer]));
+    if (rsp.status !== StatusCode.SUCCESS) throw new TransportStatusError(rsp.status);
 
-        if (rsp.dataLength !== SIGNATURE_LENGTH*hashedTx.length) throw Error('Invalid length Signature');
-        let signature = []
-        let offset = 0
-        while (offset < rsp.dataLength) {
-            const sig = rsp.data.slice(offset, offset + SIGNATURE_LENGTH)
-            offset = offset + SIGNATURE_LENGTH
-            signature.push(sig)
-        }
-        
+    if (rsp.dataLength !== SIGNATURE_LENGTH * hashedTx.length) throw Error('Invalid length Signature');
+    let signature = []
+    let offset = 0
+    while (offset < rsp.dataLength) {
+      const sig = rsp.data.slice(offset, offset + SIGNATURE_LENGTH)
+      offset = offset + SIGNATURE_LENGTH
+      signature.push(sig)
+    }
+
     for (let i = 0; i < paths.length; i++) {
       resultMap.set(paths[i].toString(true), signature[i]);
     }
